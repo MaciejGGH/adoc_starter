703a077074ab7b69b7408b67b612322774f813f2
Maciej Grabowski <maciejg@gmail.com>
2020-10-07 18:03:53 +0200

    init

---
+++ docgen.sh
@@ -0,0 +1,10 @@
+if [ -z "$2" ] || [ $2 = "html" ]; then
+    asciidoctor -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge $1
+else
+    if [ $2 = "docx" ]; then
+        asciidoctor -b docbook5 -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge --out-file - $1| \
+        pandoc --toc --from docbook --to docx --output "${1/.adoc/}".docx
+    elif [ $2 = "pdf" ]; then
+        asciidoctor-pdf -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge $1
+    fi
+fi
