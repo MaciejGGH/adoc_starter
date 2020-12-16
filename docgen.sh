6076b69da5ff51179014293fd61308f5ec0079a3
MaciejGGH <maciejg@gmail.com>
2020-12-16 10:50:41 +0100

    Add reference.docx create and comments

--- docgen.sh
+++ docgen.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+TOC_LEVEL=5
+
 if [ -z "$2" ] || [ $2 = "html" ]; then
-    asciidoctor -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge $1
+    asciidoctor -r asciidoctor-diagram -a data-uri -a toc -a toclevels=$TOC_LEVEL -a source-highlighter=rouge $1
 else
     if [ $2 = "docx" ]; then
-        asciidoctor -b docbook5 -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge --out-file - $1| \
-        pandoc --toc --from docbook --to docx --output "${1/.adoc/}".docx
+        if [ ! -f ./myref.docx ]; then
+            echo "myref.docx not found, creating default one"
+            echo "edit myref.docx style Compact, add bold"
+            echo "To add table bordersedit myref.docx, click table, design menu, expand styles, modify table style, select borders, save..."
+            pandoc --print-default-data-file reference.docx > myref.docx
+        fi
+        asciidoctor -b docbook5 -r asciidoctor-diagram -a data-uri -a toc -a toclevels=$TOC_LEVEL -a source-highlighter=rouge --out-file - $1| \
+        pandoc --reference-doc=myref.docx --toc --from docbook --to docx --output "${1/.adoc/}".docx
     elif [ $2 = "pdf" ]; then
-        asciidoctor-pdf -r asciidoctor-diagram -a data-uri -a toc -a toclevels=5 -a source-highlighter=rouge $1
+        asciidoctor-pdf -r asciidoctor-diagram -a data-uri -a toc -a toclevels=$TOC_LEVEL -a source-highlighter=rouge $1
     fi
 fi
